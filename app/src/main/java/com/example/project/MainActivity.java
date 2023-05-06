dc95800fc779c42cb7244645d595e8d9f1247e89
Carl Gösfeldt <c22cargo@student.his.se>
2023-05-06 19:15:53 +0200

    Added a button to "MainActivity", which opens "AboutActivity".

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
@@ -31,6 +34,15 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
         RecyclerView view = findViewById(R.id.recycler_view);
         view.setLayoutManager(new LinearLayoutManager(this));
         view.setAdapter(adapter);
+
+        Button button = findViewById(R.id.aboutButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AboutActivity.class);
+                startActivity(intent);
+            }
+        });
 
         new JsonTask(this).execute("https://mobprog.webug.se/json-api?login=c22cargo");
     }
