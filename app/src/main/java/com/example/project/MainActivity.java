61b5b0f6dbcc34df401fa8138280472c292869e8
Carl Gösfeldt <c22cargo@student.his.se>
2023-05-13 00:54:23 +0200

    Added an option to filter which snakes are shown in the RecyclerView.

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -6,8 +6,10 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.PopupMenu;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
@@ -43,12 +45,56 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
         view.setLayoutManager(new LinearLayoutManager(this));
         view.setAdapter(adapter);
 
-        Button button = findViewById(R.id.aboutButton);
-        button.setOnClickListener(new View.OnClickListener() {
+        Button aboutButton = findViewById(R.id.aboutButton);
+        aboutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, AboutActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        Button filterButton = findViewById(R.id.filterButton);
+
+        filterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PopupMenu filterPopUp = new PopupMenu(MainActivity.this, filterButton);
+                filterPopUp.getMenuInflater().inflate(R.menu.popup_menu, filterPopUp.getMenu());
+                filterPopUp.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+                        ArrayList<Snake> filteredItems = new ArrayList<>();
+                        switch (menuItem.getItemId()) {
+                            case R.id.noFilter:
+                                adapter.setItems(items);
+                                adapter.notifyDataSetChanged();
+                                System.out.println("TESTEST");
+                                return true;
+                            case R.id.avgOver4:
+                                for (Snake snake : items){
+                                    if (snake.getAverageLength() > 4) {
+                                        filteredItems.add(snake);
+                                    };
+                                }
+                                adapter.setItems(filteredItems);
+                                adapter.notifyDataSetChanged();
+                                return true;
+                            case R.id.maxOver6:
+                                for (Snake snake : items){
+                                    if (snake.getMaximumLength() > 6) {
+                                        filteredItems.add(snake);
+                                    };
+                                }
+                                adapter.setItems(filteredItems);
+                                adapter.notifyDataSetChanged();
+                                return true;
+                            default:
+                                return false;
+                        }
+                    }
+                });
+                filterPopUp.show();
             }
         });
 
